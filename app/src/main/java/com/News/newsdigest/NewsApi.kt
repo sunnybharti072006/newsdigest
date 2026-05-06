cec792a6012a417ecdfbcf258cff248e9a79cd68
ShockWave <sunnybharti3.142@gmail.com>
2026-05-06 13:08:55 +0530

    Update NewsApi.kt

--- app/src/main/java/com/News/newsdigest/NewsApi.kt
+++ app/src/main/java/com/News/newsdigest/NewsApi.kt
@@ -9,12 +9,12 @@ interface NewsApi {
     suspend fun getAllNews(
         @Query("q") query: String = "India",
         @Query("sortBy") sortBy: String = "publishedAt",
-        @Query("apiKey") apiKey: String = "6af99c14384c42298e2dc15a12e69311"
+        @Query("apiKey") apiKey: String = ""
     ): NewsApiResponse
 
     @GET("top-headlines")
     suspend fun getTopHeadlines(
         @Query("country") country: String = "in",
-        @Query("apiKey") apiKey: String = "6af99c14384c42298e2dc15a12e69311"
+        @Query("apiKey") apiKey: String = ""
     ): NewsApiResponse
 }
